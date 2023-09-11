d20056c9e91c171617585e42cb14496e80acabe6
laura <hernandez.sandoval.laura@gmail.com>
2023-09-11 15:47:21 -0600

    04 Operadores

---
+++ Operadores.js
@@ -0,0 +1,34 @@
+//Operadores Matematicos 
+// + - * / % ** 
+// += -= /= %= **= 
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 / 10);
+console.log(10 * 10);
+console.log(10 % 10);
+console.log(10 ** 10); //Potencia 
+
+let num = 15;
+console.log(num += 5); // num = num + 5
+console.log(num -= 5);
+console.log(num *= 5);
+console.log(num / 5);
+console.log(num %= 5);
+
+//Operadores booleanos 
+// >< >= <= !== ===
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(1 !== 2);
+console.log(1 === 2);
+
+//Operadores Logicos 
+//and &&\
+console.log (true && true);
+//or  ||
+console.log(true || false);
+//not !
+console.log(!true);
+
