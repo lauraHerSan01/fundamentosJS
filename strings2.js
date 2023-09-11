e26a6a435bead0b3cb171329ef006d91b5ee8798
laura <hernandez.sandoval.laura@gmail.com>
2023-09-11 15:59:11 -0600

    05 Metodos y Propiedades Strings

---
+++ strings2.js
@@ -0,0 +1,10 @@
+// Metodos y propiedades String 
+const text = 'EsTe eS Un TeXtO nIcE';
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(2));
+console.log(text.indexOf('T'));
+console.log(text.startsWith('T'));
+console.log(text.endsWith('E'));
+console.log('*'.repeat(10));
